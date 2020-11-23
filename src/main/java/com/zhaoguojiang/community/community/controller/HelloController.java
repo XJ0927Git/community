8b826b3568fe64ac26044dbd480289d5abeeeb4d
赵国江 <1143575690@qq.com>
2020-11-23 12:00:10 +0800

    init repo

---
+++ src/main/java/com/zhaoguojiang/community/community/controller/HelloController.java
@@ -0,0 +1,22 @@
+package com.zhaoguojiang.community.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @Author: xj0927
+ * @Description:
+ * @Date Created in 2020-11-23 11:02
+ * @Modified By:
+ */
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name = "name") String name, Model model){
+        model.addAttribute("name",name);
+        return "hello";
+    }
+}
---
+++ src/test/java/com/zhaoguojiang/community/community/CommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhaoguojiang.community.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
